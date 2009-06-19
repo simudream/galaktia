8ae6625b64345829648b6c81a074fc7209f5108d
manuelaraoz <manuelaraoz@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-06-19 23:03:15 +0000

    commit intermedio... haciendo el mini cliente con frontend básica

--- trunk/galaktia/galaktia/server/protocol/tests/chat.py
+++ trunk/galaktia/galaktia/server/protocol/tests/chat.py
@@ -8,54 +8,18 @@ from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 from twisted.python import log
 
+from baseChat import *
+
 logger = logging.getLogger(__name__)
 
-class BaseMessageProtocol(DatagramProtocol):
-    """ An abstract base class for remote object messaging """
 
-    def datagramReceived(self, input_data, (host, port)):
-        """ Event handler for datagram reception """
-        input_message = self.decode(input_data)
-        logger.debug('Received from %s:%d: %s', host, port, input_message)
-        for output_message, dst in self.process(input_message, (host, port)):
-            self.send(output_message, dst)
 
-    def send(self, output_message, dst=None):
-        """ Sends an output message to given destination (host, port) """
-        self.transport.write(self.encode(output_message), dst)
-        logger.debug('Sent to %s: %s', dst or 'server', output_message)
-
-    def process(input_message, host, port):
-        """ Processes input message (from host:port) into an output message """
-        raise NotImplementedError
-
-    def decode(self, input_data):
-        """ Decompresses and deserializes input message data into an object """
-        return simplejson.loads(input_data.decode('zlib'))
-
-    def encode(self, output_message):
-        """ Serializes and compresses an object into an output message data """
-        return simplejson.dumps(output_message, \
-                separators=(',', ':')).encode('zlib')
-
-class ChatServerProtocol(BaseMessageProtocol):
-    """ Implementation of a simple echo chat server """
-
-    def process(self, input_message, (host, port)):
-        """ Implements processing by returning output identical to input """
-        return [(input_message+' server salted', (host, port))] 
-                # extensible for more logic
-
-user_list = []
+        
 class ManuServerProtocol(ChatServerProtocol):
     """ Implementation of basic chat server """
     
-    #def __init__(self):
-        
-        #ChatServerProtocol.__init__(self)
-        #super(self, ManuServerProtocol).__init__()
-        
-    
+    userList = []
+
     def process(self, input_message, (host,port)):
         """ Implements processing by parsing input message"""
         dst = (host,port)
@@ -66,17 +30,17 @@ class ManuServerProtocol(ChatServerProtocol):
         command, argument = parsed_input[0], parsed_input[1]
         
         if command == "new_user":
-            if argument not in [x[0] for x in user_list]:
-                user_list.append( [argument,dst] )
+            if argument not in [x[0] for x in self.userList]:
+                self.userList.append( [argument,dst] )
                 logger.debug('New user @%s:%d: %s', host, port, argument)
                 return [("user:accepted", (host,port))]
             else:
                 return [("user:rejected",(host,port))]
         else:
-            if command in [x[0] for x in user_list]:
+            if command in [x[0] for x in self.userList]:
                 logger.debug('%s said %s' % (command,argument))
                 lista = []
-                for user_tuple in user_list:
+                for user_tuple in self.userList:
                     lista.append((str(command+': '+argument), user_tuple[1]))
                 return lista
             else:
@@ -84,46 +48,6 @@ class ManuServerProtocol(ChatServerProtocol):
                 
 
 
-class ChatClientProtocol(BaseMessageProtocol):
-    """ Implementation of a simple chat client (unreliable protocol) """
-
-    DEFAULT_SERVER = '192.168.1.1'
-    DEFAULT_PORT = 6414
-
-    def __init__(self, server=DEFAULT_SERVER, port=DEFAULT_PORT):
-        """ ChatClientProtocol constructor """
-        self.server = server
-        self.port = int(port)
-
-    def startProtocol(self):
-        """ Triggers the first message sent by client on a session """
-        self.transport.connect(self.server, self.port)
-        sys.stdout.write('Connected to server %s\n' % self.server)
-        sys.stdout.write('Enter text to chat with the echo server')
-        output_message = self.prompt()
-        self.send(output_message or '')
-
-    def connectionRefused(self):
-        """ Event handler for refused connections (no server listening) """
-        logger.error('No server listening at %s:%d', self.server, self.port)
-
-    def process(self, input_message, (host, port)):
-        """ Writes server response and prompts for a new message to send """
-        sys.stdout.write(input_message)
-        output_message = self.prompt()
-        if output_message is None:
-            reactor.stop() # the reactor singleton is not a good idea...
-            return []
-        return [(output_message, None)]
-
-    def prompt(self):
-        """ Prompts to read a new message to be sent to the server """
-        sys.stdout.write('\n> ')
-        output_message = sys.stdin.readline() # prompts user for input
-        output_message = output_message.rstrip('\n')
-        if output_message == 'quit':
-            return None # exits on empty message or by entering 'quit'
-        return output_message
 
 class ManuClientProtocol(ChatClientProtocol):
     username = ''
@@ -176,21 +100,31 @@ class ManuClientProtocol(ChatClientProtocol):
             reactor.stop() # the reactor singleton is not a good idea...
             return []
         return [(self.username+':'+output_message, None)]
-
-def main(program, endpoint='client', host='127.0.0.1', port=6414):
-    """ Main program: Starts a chat client or server on given host:port """
-    if endpoint == 'client':
-        logging.basicConfig(stream=sys.stderr, level=logging.INFO)
-        protocol = ChatClientProtocol(host, port)
-        port = 0 # dinamically assign client port
-    elif endpoint == 'server':
-        logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-        protocol = ChatServerProtocol()
-    elif endpoint == 'mclient':
+        
+        
+class ClientInterface(object):
+    
+    def __init__(self):
+        endpoint='client'
+        host='127.0.0.1'
+        port = 6414
         logging.basicConfig(stream=sys.stderr, level=logging.INFO)
         protocol = ManuClientProtocol(host, port)
         port = 0 # dinamically assign client port
-    elif endpoint == 'mserver':
+        logger.info('Starting %s', endpoint)
+        reactor.listenUDP(port, protocol)
+        
+    def run(self):
+        reactor.run()
+
+def main(program, endpoint='client', host='127.0.0.1', port=6414):
+    """ Main program: Starts a chat client or server on given host:port """
+
+    if endpoint == 'client':
+        logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+        protocol = ManuClientProtocol(host, port)
+        port = 0 # dinamically assign client port
+    elif endpoint == 'server':
         logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
         protocol = ManuServerProtocol()
     else: # bad arguments
