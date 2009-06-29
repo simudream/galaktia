bf57dbf0b055ebb67b1d208193c93fac6638a0be
manuelaraoz <manuelaraoz@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-06-29 05:58:02 +0000

    agregue operacion exit bugfix del server

---
+++ galaktia/galaktia/server/protocol/operations/exit.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, logging
+
+from galaktia.server.protocol.operations.action import ActionRequest, \
+                        ActionResponse, ActionUpdate
+from galaktia.server.protocol.model import Acknowledge
+
+
+"""
+***********************************************************************
+                               Exit
+***********************************************************************
+Client:                                                     Server:
+            StopPlayerSession    - >
+
+                                < -  UserExited[All]
+
+            UserExitedAck        - > 
+
+- StopPlayerSession: El cliente le envía al server un mensaje con su 
+session_id dicéndole que quiere cerrar la sesión de ese personaje.
+
+- UserExited: El server le envía un mensaje a todos los jugadores 
+(incluído el que quiere irse) diciendo que el jugador se va del juego.
+Incluye el identificador del jugador que se va. Al recibir este mensaje,
+el cliente debe eliminarlo de su registro interno de jugadores en línea
+de visión o hacer todos los cambios necesarios para eliminar completamente
+el jugador del mundo virtual de galaktia.
+"""
+
+class StopPlayerSession(ActionRequest):
+    """ C->S Command for p2p chatting"""
+    def __init__(self, **kwargs):
+        kwargs['action'] = None
+        kwargs['object'] = None
+        ActionRequest.__init__(self, **kwargs)
+
+class UserExited(ActionUpdate):
+    """ S->C Command for informing client that other client said something
+        in chat. """
+    def __init__(self, **kwargs):
+        kwargs['subject'] = kwargs['username']
+        kwargs['action'] = None
+        kwargs['object'] = None
+        ActionUpdate.__init__(self, **kwargs)
+
+class UserExitedAck(Acknowledge):
+    """ C->S Acknowledge for UserExited Action"""
+    pass
+
+
+
+
+
