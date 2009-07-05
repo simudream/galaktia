66e1fa1f6f7c078c80e75eb2d832bcc494d9d92a
williams.matias <williams.matias@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-07-05 22:58:55 +0000

    Haciedno todo en pyglet

--- trunk/galaktia/galaktia/client/controller/refactor.py
+++ trunk/galaktia/galaktia/client/controller/refactor.py
@@ -1,8 +1,68 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import wx
 import os
 import pyglet
 from pyglet.window import key
+
+
+class GalaktiaViewport(pyglet.graphics.Batch):
+
+    IMAGES_DIR = os.path.join(os.path.dirname( \
+            os.path.abspath(__file__)), os.pardir, 'assets', 'images')
+
+    def __init__(self):
+        super(GalaktiaViewport, self).__init__()
+        self.background = pyglet.graphics.OrderedGroup(0)
+        self.foreground = pyglet.graphics.OrderedGroup(1)
+        image = pyglet.image.load(os.path.join(self.IMAGES_DIR, 'walter.gif'))
+        self.sprites = [
+            pyglet.sprite.Sprite(image, batch=self, group=self.foreground)
+        ]
+        walter = self.sprites[0] # TODO: quick'n'dirty
+        walter.x, walter.y = 380, 180
+
+class PygletPanel(pyglet.window.Window,wx.Panel):
+
+    def __init__(self):
+        super(PygletPanel, self).__init__(caption='Galaktia')
+        self.label = pyglet.text.Label(u'¡Bienvenido a Galaktia!',
+                font_name='Arial', font_size=36, bold=True,
+                x=self.width//2, y=self.height//2,
+                anchor_x='center', anchor_y='center')
+        self.keystate = key.KeyStateHandler()
+        self.push_handlers(self.keystate)
+        self.viewport = GalaktiaViewport()
+
+    def on_draw(self):
+        self.clear()
+        self.draw_gl_rumble()
+        self.label.draw()
+        self.viewport.draw()
+
+    def draw_gl_rumble(self):
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                ('v2i', (300, 300, 500, 200, 300, 100, 100, 200)),
+                ('c3B', (0, 0, 255, 255, 0, 0, 255, 255, 0, 0, 255, 0))
+        )
+
+    def on_key_press(self, symbol, modifiers):
+        print 'A key was pressed: %s %s' % (symbol, modifiers)
+        if symbol == pyglet.window.key.ESCAPE:
+            self.dispatch_event('on_close')
+
+    def on_text_motion(self, motion):
+        STEP = 20
+        motion_codes = [0, -1, 1, 0] # none, lower, upper, both
+        decode = lambda lower, upper: motion_codes[self.keystate[lower] \
+                | (self.keystate[upper] << 1)] * STEP
+        dx, dy = (decode(key.LEFT, key.RIGHT), decode(key.DOWN, key.UP))
+        if dx or dy:
+            walter = self.viewport.sprites[0] # TODO: quick'n'dirty
+            walter.x += dx
+            walter.y += dy
+            self.dispatch_event('on_draw')
+
 
 class LoginPanel(wx.Panel):
     def __init__(self,parent,colour):
@@ -60,14 +120,17 @@ class LoginPanel(wx.Panel):
             self.parent.SetStatusText('Ingrese un nombre y password válidos')
             return	
         self.parent.SetStatusText('Logueando')
-        username = self.usernameInput.GetValue()
-        password = self.passwordInput.GetValue()
-        self.parent.SetStatusText('Logueado como: '+ username + ". Su password es: " + password)
+        self.parent.username = self.usernameInput.GetValue()
+        self.parent.password = self.passwordInput.GetValue()
+        self.parent.SetStatusText('Logueado como: '+ self.parent.username + ". Su password es: " + self.parent.password)
+        self.parent.logged()
         return    
 	
 class NudeFrame(wx.Frame):
     def __init__(self, parent, title, colour):
-        wx.Frame.__init__(self, parent, -1, title, pos=(200, 200), size=(500, 500))
+        wx.Frame.__init__(self, parent, -1, title, pos=(200, 200), size=(800, 800))
+        self.username = None
+        self.password = None
         self.SetBackgroundColour(colour)
         IMAGES_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, 'assets', 'images')
         icon = wx.Icon(os.path.join(IMAGES_DIR, 'logo2.jpg'), wx.BITMAP_TYPE_ANY)
@@ -79,12 +142,24 @@ class NudeFrame(wx.Frame):
         menuBar.Append(menu, "&File")
         self.SetMenuBar(menuBar)
         self.CreateStatusBar()
-        panel = LoginPanel(self,colour)
-        box = wx.BoxSizer(wx.VERTICAL)
-        panel.SetBackgroundColour(colour)
-        box.Add(panel, wx.EXPAND | wx.ALL, 3)
-        self.SetSizer(box)
+        self.panel = LoginPanel(self,colour)
+        self.box = wx.BoxSizer(wx.VERTICAL)
+        self.panel.SetBackgroundColour(colour)
+        self.box.Add(self.panel, wx.EXPAND | wx.ALL, 3)
+        self.Layout()
+        self.SetSizer(self.box)
         
+    def logged(self):
+        self.SetBackgroundColour(wx.RED)
+        self.panel.Destroy() 
+        self.panel = PygletPanel()
+        self.panel.SetBackgroundColour(wx.RED)
+        self.panel.Fit()
+        self.box.Add(self.panel, wx.EXPAND | wx.ALL, 3)
+        self.Layout()
+        
+        
+                
     def OnQuit(self, evt):
         self.Close()
 	    
