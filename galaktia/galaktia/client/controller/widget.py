40f2d3efc7d90e0d030349c0dd801829f3a2155e
williams.matias <williams.matias@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-08-04 23:05:46 +0000

    Tenemos password

--- galaktia/galaktia/client/controller/widget.py
+++ galaktia/galaktia/client/controller/widget.py
@@ -42,27 +42,48 @@ class TextWidget(object):
     def empty(self):
         self.document.text = ""
 
+class myCaret(pyglet.text.caret.Caret):
+    def __init__(self, layout):
+        super(myCaret,self).__init__(layout)
+        
+    def on_text(self,text):
+        super(myCaret,self).on_text('*'*len(text))
+        self._layout.document.insert_password_text(self._position,text)
+
+        
+
 class PasswordDocument(pyglet.text.document.UnformattedDocument):
     def __init__(self, text, field):
         self.field = field
         super(PasswordDocument,self).__init__(text)
 
-    def insert_text(self,start,text):
-        print 'a'
+    def insert_password_text(self,start,text):
         self.field.passwd = self.field.passwd[:start] + text + self.field.passwd[start:]
-        self.text = '*'*len(self.text)
-        super(PasswordDocument,self).insert_text(start,'*'*len(text))
-
+        print 'add:'+self.field.passwd
+        
     def delete_text(self,start,end):
-        self.field.passwd = self.field.passwd[:start] + self.field.password[end:]
-        self.text = '*'*len(self.text)
+        self.field.passwd = self.field.passwd[:start] + self.field.passwd[end:]
         super(PasswordDocument,self).delete_text(start,end)
+        print 'delete:'+self.field.passwd
+        
+        
+        
     
 class PasswordField(TextWidget):
     def __init__(self, text, x, y, width, batch):
         super(PasswordField,self).__init__(text,x,y,width,batch)
-        self.passwd = text
         self.document = PasswordDocument(text,self)
+        self.document.set_style(0, len(self.document.text), 
+            dict(color=(0, 0, 0, 255))
+        )
+        font = self.document.get_font()
+        height = font.ascent - font.descent
+        self.layout = pyglet.text.layout.IncrementalTextLayout(
+            self.document, width, height, multiline=False, batch=batch)
+        self.layout.x = x
+        self.layout.y = y
+        self.passwd = text
+        self.caret = myCaret(self.layout)
 
     def text(self):
         return self.passwd
