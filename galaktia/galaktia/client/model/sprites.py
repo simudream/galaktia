26cf81d4bc2fa5ea7fecc1d54929fcae8db4ec50
eordano <eordano@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-09-26 02:22:45 +0000

    Pequeños cambios, lo que comenté antes:
    Cuando le decimos a los sprites que se dibujen, le estamos pasando 4 
    parámetros que son siempre los mismos (cuánto padding y tamaño del tile)
    
    A pesar que es un antipatrón de diseño, al ser una función que es llamada 
    (FPS*Cantidad de objetos) por segundo, me parece que no dá para pasar los 
    parámetros cada vez que dibujamos un sprite (que en esta versión son del 
    orden de los 200 objetos y eso que no tenemos prácticamente nada en 
    pantalla)

--- galaktia/galaktia/client/model/sprites.py
+++ galaktia/galaktia/client/model/sprites.py
@@ -10,22 +10,20 @@ from galaktia.client.paths import IMAGES_DIR
 
 class GameView(object):
     def __init__(self, map_dim, tile_width, tile_height, padding_left, padding_down, surroundings):
-        self.tile_width = tile_width
-        self.tile_height = tile_height
-
-        self.padding_left = padding_left
-        self.padding_down = padding_down
+        self.tile_size = {'x':tile_width, 'y':tile_height}
+        self.padding = {'x':padding_left, 'y':padding_down}
 
         self.walter = pyglet.image.load(os.path.join(IMAGES_DIR, 'walter.png'))
         self.red_walter = pyglet.image.load(os.path.join(IMAGES_DIR, 'red_walter.png'))
         self.piso = pyglet.image.load(os.path.join(IMAGES_DIR, 'piso.png'))
         self.pared = pyglet.image.load(os.path.join(IMAGES_DIR, 'pared.png'))
         
-        self.mapa = [Pared((t[0],t[1]), self.pared) for t in surroundings]
+        self.mapa = [Pared((t[0],t[1]), self.pared, self.tile_size, self.padding) \
+                     for t in surroundings]
         self.baldosas = []
         for x in xrange(map_dim):
             for y in xrange(map_dim):
-                self.baldosas.append(Baldosa((x,y), self.piso))
+                self.baldosas.append(Baldosa((x,y), self.piso, self.tile_size, self.padding))
         self.peers = {}
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
@@ -33,46 +31,43 @@ class GameView(object):
         self.own_walter = None
 
     def draw(self):
-        self.center_walter['x'] = self.own_walter.x
-        self.center_walter['y'] = self.own_walter.y
-
+        self.center_walter = {'x': self.own_walter.x, 'y': self.own_walter.y}
         for baldosa in self.baldosas:
-            baldosa.draw(self.tile_width, self.tile_height, \
-                self.padding_left, self.padding_down, self.center_walter)
-
+            baldosa.draw(self.center_walter)
         for pared in self.mapa:
-            pared.draw(self.tile_width, self.tile_height, \
-                self.padding_left, self.padding_down, self.center_walter)
-
+            pared.draw(self.center_walter)
         for aSession in self.peers:
-            walter = self.peers[aSession]
-            if walter == self.own_walter:
-                walter.draw(self.tile_width, self.tile_height, \
-                self.padding_left, self.padding_down, self.center_walter)
-            else:
-                walter.draw(self.tile_width, self.tile_height, \
-                            self.padding_left, self.padding_down, self.center_walter)
+            self.peers[aSession].draw(self.center_walter)
 
     def delete_player(self, session_id):
         del self.peers[session_id]
 
     def add_player(self, session_id, (x,y), description, is_me):
         image = self.red_walter if is_me else self.walter
-        player = Walter((x,y), description, image)
+        player = Walter((x,y), description, image, self.tile_size, self.padding)
         self.peers[session_id] = player
         if is_me:
             self.center_walter['x'] = x
             self.center_walter['y'] = y
             self.own_walter = player
+
+
 class Sprite(object):
-    def __init__(self, (x,y), image):
+    def __init__(self, (x,y), image, tile_size, padding):
         self.x, self.y = x,y
         self.image = image
+        self.tile_size = tile_size
+        self.padding = padding
 
-    def draw(self, tile_width, tile_height, padding_left, padding_down, center):
+    def draw(self, center):
         iso_x = (self.y - center['y'] + self.x - center['x'])*0.91
         iso_y = (self.y - center['y'] - self.x + center['x'])*0.91
-        self.image.blit(tile_width*iso_x+padding_left, tile_height*iso_y+padding_down)
+        self.image.blit(self.tile_size['x']*iso_x+self.padding['x'], 
+            self.tile_size['y']*iso_y+self.padding['y'])
+
+    def cambiar_setup(self, tile_size, padding):
+        self.tile_size = tile_size
+        self.padding = padding
 
 
 class Walter(Sprite):
