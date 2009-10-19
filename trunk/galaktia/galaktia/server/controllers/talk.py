ccae5af99999c1422c425c9860efc108e85d2c59
elementohb <elementohb@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-10-19 02:57:03 +0000

    Correcto uso de Session en server, ahora es inmutable y no tiene atributo de mas. Y un poco de clean-up a ProtocolCodec.

--- trunk/galaktia/galaktia/server/controllers/talk.py
+++ trunk/galaktia/galaktia/server/controllers/talk.py
@@ -10,12 +10,23 @@ class SayThisController(MessageController):
     
     MESSAGE_KEYS = ['action']
     
+    def __init__(self, session_dao, dao_resolver):
+        
+        MessageController.__init__(self, session_dao, dao_resolver)
+        
+        self.char_dao = self.dao_resolver.char
+        self.user_dao = self.dao_resolver.user
+    
     def _process(self, session, action):
+        
+        character = self.char_dao.get_by(id=session.character_id)
+        user = self.user_dao.get_by(id=character.user_id)
+        username = user.name
         
         for aSession in self.session_dao.get_logged():
             m = SomeoneSaid(
-                username = session.user.name,
-                message = action, 
-                session = aSession
+                username=username,
+                message=action, 
+                session=aSession
                 )
             yield m
