c89daf84b66b97accebdf634be14fe8d585637c7
dariosn <dariosn@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-10-20 21:22:08 +0000

    optimizacion, clarificacion, simplificacion y mejora estetica de la ex- horrible funcion get_angle PD: comela esteban

--- trunk/galaktia/galaktia/client/get_angle.py
+++ trunk/galaktia/galaktia/client/get_angle.py
@@ -1,49 +1,11 @@
-import math
+# -*- coding: utf-8 -*-
+
+def cross( (x1,y1) , (x2,y2) ):
+	return x1*y2 - x2*y1
 
 def get_angle(x, y):
-    angle = math.atan(float(y)/float(x)) \
-        if x != 0 else math.atan(float(y)/float(0.0001))
-    if x > 0:
-        if y > 0:
-            # Cuadrante 1
-            if math.pi/8 < angle < 3*math.pi/8:
-                retval = 1
-            elif angle >= 3*math.pi/8:
-                retval = 0
-            else:
-                retval = 2
-        elif y == 0:
-            # Eje X
-            retval = 2
-        else:
-            # Cuadrante 4
-            if -math.pi/8 > angle > -3*math.pi/8:
-                retval = 3
-            elif angle <= -3*math.pi/8:
-                retval = 4
-            else:
-                retval = 2
-    elif x == 0:
-        # Eje Y
-        retval = 0 if y > 0 else 4
-    else:
-        if y > 0:
-            # Cuadrante 2
-            if -angle > 3*math.pi/8:
-                retval = 0
-            elif -angle < math.pi/8:
-                retval = 6
-            else:
-                retval = 7
-        elif y == 0:
-            # Eje X
-            retval = 6
-        else:
-            # Cuadrante 3
-            if math.pi/8 < angle < 3*math.pi/8:
-                retval = 5
-            elif math.pi/8 >= angle:
-                retval = 6
-            else:
-                retval = 4
-    return retval
+	P = ( (-1,2),(1,2),(2,1),(2,-1),(1,-2),(-1,-2),(-2,-1),(-2,1),(-1,2) )
+	for i in range(0,8):
+		if( cross( P[i], (x,y) ) < 0 and cross( (x,y) , P[i+1] ) <= 0 ):
+			return i
+		return 'X'
