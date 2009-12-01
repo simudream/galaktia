b2c90a78155c0c484c20a25c47358513861f0c5e
rhfixer <rhfixer@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2009-12-01 04:38:50 +0000

    El MemoryPool en Python más largo que escribí en mi vida. Faltan los tests.

--- trunk/galaktia/galaktia/server/persistence/memory.py
+++ trunk/galaktia/galaktia/server/persistence/memory.py
@@ -26,6 +26,9 @@ class NotSet(object):
     """
     pass
 
+class OutOfBounds(Exception):
+    pass
+
 #
 #   PSEUDO-INTERFACES
 #
@@ -53,19 +56,22 @@ class MemoryPool(Memory):
     """
         A MemoryPool object provides an extended way of using Memory gateways.
         It holds more than one connection open, so it should be theoretically
-        faster than ad-hoc connections.
+        faster than ad-hoc connections. However, it does not necessarily imply
+        that the objects in the pool are connected to different servers.
 
-        It *must* behave like a Memory gateway too.
+        It *must* behave like a Memory gateway too, masking the
+        lock/hold/return operations inside the MemoryPool, providing a complete
+        replacement for normal Memory objects.
     """
+
     def count(self):
         raise NotImplementedError
 
     def grow(self, number=1):
         raise NotImplementedError
 
-    def reduce(self, number=1):
+    def shrink(self, number=1):
         raise NotImplementedError
-
 
 #
 #   DECORATORS AND FACTORIES
@@ -121,7 +127,7 @@ class MemcacheMemory(Memory):
         Memory gateway to a Memcache server
     """
 
-    def __init__(self, servers=["127.0.0.1:11211"], expire=None, DEBUG=False):
+    def __init__(self, servers=["127.0.0.1:11211"], expire=0, DEBUG=False):
         """
             :param servers: List of servers to use. Please, read
             memcache.Client help.
@@ -148,17 +154,85 @@ class MemcacheMemory(Memory):
         self.log.debug("Setting key")
         if value is None:
             value = NotSet()
-        self._client.set(key, value)
+        self._client.set(key, value, self._expire)
 
     def __delitem__(self, key):
         self.log.debug("Deleting key %s", key)
         if self._client.delete(key) == 0:
             raise KeyError
-            
-            
-class MemcacheMemoryPool(MemoryPool):
-    pass
 
+
+class MemcacheMemoryPool(MemoryPool):
+    def __init__(self, servers=["127.0.0.1:11211"], expire=0, upper_limit=100, \
+            lower_limit=1, DEBUG=False):
+        self._clients = [MemcacheMemory(servers=servers, expire=expire) for i
+                in xrange(0, upper_limit/2)]
+        self.__expire = expire
+        self._servers = servers
+        self.upper_limit = upper_limit
+        self.lower_limit = lower_limit
+        logging.basicConfig(level=logging.WARNING)
+        self.log = logging.getLogger("Memcache Pool")
+        self.__DEBUG = DEBUG
+        if DEBUG:
+            self.log.setLevel(logging.DEBUG)
+
+    @property
+    def _expire(self):
+        def fget(self):
+            return self.__expire
+        def fset(self, value):
+            if self.__expire != value:
+                self.__expire = value
+                for i in self._clients:
+                    i._expire = self.value
+
+    def count(self):
+        return len(self._clients)
+
+    def grow(self, number=1):
+        self.log.debug("Adding %s new servers to the pool", number)
+        for i in range(number):
+            self._clients.append[MemcacheMemory(self._servers, self._expire,
+                    self.__DEBUG)]
+
+    def shrink(self, number=1):
+        self.log.debug("Deleting %s servers from the pool", number)
+        for i in range(number):
+            self._clients.pop()
+
+    def __claim_client(self):
+        if self.count() < self.lower_limit:
+            self.grow()
+        return self._clients.pop()
+
+    def __return_client(self, client):
+        if self.count() < self.upper_limit:
+            self._clients.append(client)
+
+    def __getitem__(self, key):
+        self.log.debug("Accessing key %s", key)
+        try:
+            client = self.__claim_client()
+            return client[key]
+        finally:
+            self.__return_client(client)
+
+    def __setitem__(self, key, value):
+        self.log.debug("Setting key %s to %s", key, value)
+        try:
+            client = self.__claim_client()
+            client[key] = value
+        finally:
+            self.__return_client(client)
+
+    def __delitem__(self, key):
+        self.log.debug("Deleting key %s", key)
+        try:
+            client = self.__claim_client()
+            client.__delitem__(key)
+        finally:
+            self.__return_client(client)
 
 class RedisMemory(Memory):
     """
@@ -202,7 +276,7 @@ class RedisMemory(Memory):
         self.log.debug("Deleting key %s", key)
         if self._client.delete(key) == 0:
             raise KeyError
-        
+
     def expire(self, key, time):
         self.log.debug("Setting expire time to %s seconds for key %s", time, key)
         self._client.expire(key, time)
@@ -215,7 +289,7 @@ class RedisMemory(Memory):
 
 
 if __name__ == "__main__":
-    memory = MemcacheMemory(DEBUG=True)
+    memory = MemcacheMemoryPool(DEBUG=True)
     @memorize(memory)
     def test(a):
         """test test test!!!"""
