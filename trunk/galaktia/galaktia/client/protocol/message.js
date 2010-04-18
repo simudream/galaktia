1aa886e586bb815ce765a374f41345bfdd956045
elementohb <elementohb@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-04-18 19:42:31 +0000

    More work on client. Adding controllers and improved dispatcher & message.

--- trunk/galaktia/galaktia/client/protocol/message.js
+++ trunk/galaktia/galaktia/client/protocol/message.js
@@ -6,11 +6,28 @@ Galaktia.Message = new Class({
 	normalize: function() {
 		var normal = {};
 		
-		for ( key in attributes ) {
+		for ( var i = 0; i < attributes.length; i++ ) {
 			
-			normal[attributes[key]] = this[attributes[key]];
+			normal[attributes[i]] = this[attributes[i]];
 		}
 		
+		normal.type = this.type;
 		return normal;
+	},
+	intialize: function(literal) {
+		
+		var values;
+		if ( $defined(literal) ) {
+			
+			values = $extend(this.defaultValues, literal);
+		} else {
+			
+			values = this.defaultValues;
+		}
+		
+		$each(values, function(value, prop){
+			
+			this[prop] = value;
+		});
 	}
 });
