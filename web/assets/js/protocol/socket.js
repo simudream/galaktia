3b5a4e44405b355fc68f989e9e9eb1643ba90492
elementohb <elementohb@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-04-21 00:12:43 +0000

    El asesino de scoffey me habia borrado todo!! Ver revisiones 220 y 221.

---
+++ web/assets/js/protocol/socket.js
@@ -0,0 +1,41 @@
+var Galaktia = Galaktia || {};
+
+Galaktia.JSONSocket = new Class({
+	initialize: function(url) {
+		
+		this.url = url;
+		this.ws = new WebSocket(this.url);
+		this.handler = function(message) {};
+	},
+	registerListenHandler: function(handler) {
+	
+		if ( $type(handler) == 'function' ) {
+			
+			this.handler = handler;
+			this.ws.onmessage = function(message) {
+				
+				handler(JSON.parse(message.data));
+			};
+		}
+	},
+	send: function(message) {
+		
+		if ( message instanceof Galaktia.Message ) {
+			
+			message = message.normalize();
+		}
+		
+		this.ws.send(JSON.stringify(message));
+	},
+	reopen: function(url) {
+		
+		if ( this.ws.readyState != WebSocket.CLOSED ) {
+			
+			this.ws.close();
+		}
+		
+		this.url = url || this.url;
+		this.ws = new WebSocket(this.url);
+		this.registerListenHandler(this.handler);
+	}
+});
