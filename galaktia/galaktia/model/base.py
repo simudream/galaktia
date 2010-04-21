4baaa1c4ac13ca0b634ecec074638121a8b4c857
santiago.coffey <santiago.coffey@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-04-21 19:58:15 +0000

    Enhancements in WebSocketServer, WebSocketRequestHandler and RequestHandlerAdapter.
    Test-drive implementation of a SessionDAO for RequestHandlerAdapter.
    
    Minor updates in galaktia.message and controlfreak configuration.
    
    Enhancements in DispatcherController: Dispatcher routes are now better detected via pkgutils.iter_modules and subclass lookups instead of matching module and class name patterns. Also warns about detection failures. Thanks slezica89 for the suggestion! Now delete your dispatcher implementation! xD

--- galaktia/galaktia/model/base.py
+++ galaktia/galaktia/model/base.py
@@ -2,64 +2,90 @@
 # -*- coding: utf-8 -*-
 
 """
-Base server model and implementation.
+Base model for server request handlers.
 """
 
 import logging
 
 from mod_pywebsocket import msgutil
 
-from galaktia.model.standalone import WebSocketRequestHandler
+from galaktia.model.standalone import WebSocketServer, WebSocketRequestHandler
 
 logger = logging.getLogger(__name__)
 
-class BaseServer(WebSocketRequestHandler):
-    """ Base class for a web sockets server """
+def make_request_handler_factory(codec, controller, session_dao):
+    """ :return: callable that returns RequestHandlerAdapter instances
+                 configured with given args """
+    def request_handler_factory(*args, **kwargs):
+        rh = RequestHandlerAdapter(*args, **kwargs)
+        rh.codec = codec
+        rh.controller = controller
+        rh.session_dao = session_dao
+        return rh
+    return request_handler_factory
 
-    def __init__(self, codec, controller):
-        """
-        BaseServer constructor.
+class RequestHandlerAdapter(WebSocketRequestHandler):
+    """ Adapts WebSocketRequestHandler to galaktia.model """
 
-        :parameters:
-            codec : Codec
-                Converts ``str`` to ``Message`` and viceversa
-            controller : Controller
-                Implements a message handling method
-        """
-        self.codec = codec
-        self.controller = controller
-        self._receive = None
-        self._send = None
+    codec = None
+    """ :ivar: ``Codec`` that converts ``str`` to ``Message`` and viceversa """
+    controller = None
+    """ :ivar: ``Controller`` that handles ``Message`` instances """
+    session_dao = None
+    """ :ivar: ``SessionDAO`` that maps sessions to requests """
 
     def do_extra_handshake(self, request):
         """ Performs extra validation on handshake """
-        pass # always accept # TODO: do extra login validation
+        pass # always accept # TODO: do extra login validation?
 
     def transfer_data(self, request):
         """ Runs web socket request handler main loop """
+        # NOTE: request == self._request
         while True:
             try:
-                self.handle(request)
+                self._transfer_data()
             except StopIteration:
                 break
             except Exception:
                 logger.exception('Fatal error handling request: %r', request)
 
-    def handle(self, request):
+    def _transfer_data(self):
         """ Performs a receive/send step of the request handler main loop  """
-        input_string = msgutil.receive_message(request)
+        input_string = msgutil.receive_message(self._request)
         input_message = self.codec.decode(input_string)
-        self.receive(request, input_message)
+        for output_message in self.receive(input_message):
+            self.send(output_message)
 
-    def receive(self, request, input_message):
+    def receive(self, input_message):
         """ Receives an input message to be handled by controller """
+        request = self._request
+        input_message._src_session = self.session_dao.get_session(request)
         logger.debug('Received from %r: %s', request, input_message)
-        for output_message in self.controller.handle(input_message):
-            self.send(request, output_message)
+        return self.controller.handle(input_message)
 
-    def send(self, request, output_message):
+    def send(self, output_message):
         """ Sends output messages returned by controller """
+        request = self.session_dao.get_request(output_message, self._request)
         output_string = self.codec.encode(output_message)
         msgutil.send_message(request, output_string)
         logger.debug('Sent to %r: %s', request, output_message)
+
+class SessionDAO(dict):
+    """ Session DAO. Maps sessions to requests """
+
+    # Not sure if all this works...
+
+    def get_request(self, message, current_request):
+        if isinstance(message, ExitResponseMessage):
+            raise StopIteration('Request finished (socket to be closed)')
+        if current_request not in self:
+            if message._src_session != message._dst_session:
+                raise ValueError('First message of a new session must be ' \
+                        'sent to the same session')
+            self[message._src_session] = current_request
+            current_request._session = message._src_session
+        return self[message._dst_session]
+
+    def get_session(self, request):
+        return request._session
 
--- galaktia/galaktia/model/dispatcher.py
+++ galaktia/galaktia/model/dispatcher.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python2.6
 # -*- coding: utf-8 -*-
 
+import logging
 import os
+from pkgutil import iter_modules
 
-from galaktia.model.controller import BaseDispatcherController
+import galaktia.controller
+from galaktia.model.controller import Controller, BaseDispatcherController
+from galaktia.model.message import RequestMessage
+
+logger = logging.getLogger(__name__)
 
 class DispatcherController(BaseDispatcherController):
-    """ A dispatcher controller that can handle messages whose class name
-        ends with "RequestMessage" by dispatching them to the controller
-        whose class has the same name but ends with "Controller". """
+    """ A dispatcher controller that can handle messages of a
+        ``RequestMessage`` subclass by dispatching them to the
+        controller of a ``Controller`` subclass in the same module. """
 
-    # Mmmmm... Not very elegant... Dispathing by class name is a bit hacky :P
-
-    BASE_MODULE = 'galaktia.controller'
-    CONTROLLER_NAME = '%sController'
-    MESSAGE_NAME = '%sRequestMessage'
-
-    def __init__(self, dao, base_module=BASE_MODULE, **extra_routes):
+    def __init__(self, dao, package=galaktia.controller, **extra_routes):
         self.routes = extra_routes or {}
-        for module in self._locate_submodules(base_module):
-            name = module.title()
-            imported = __import__('.'.join((base_module, module)), \
-                    fromlist=[self.CONTROLLER_NAME % name])
-            controller = getattr(imported, self.CONTROLLER_NAME % name)()
-            controller.dao = dao
-            self.routes[self.MESSAGE_NAME % name] = controller
+        for i in iter_modules(package.__path__, package.__name__ + '.'):
+            try:
+                module = __import__(i[1], fromlist=['__name__'])
+                controller = self._find_subclass(module, Controller)()
+                message_class = self._find_subclass(module, RequestMessage)
+                controller.dao = dao
+                self.routes[message_class.__name__] = controller
+            except Exception, e:
+                logger.warn('Skipped dispatcher routing for module %r: %s', \
+                        i[1], e)
 
-    def _locate_submodules(self, base_module):
-        imported = reduce(lambda imported, path: getattr(imported, path), \
-                base_module.split('.')[1:], __import__(base_module))
-        path = os.path.abspath(os.path.dirname(imported.__file__))
-        for filename in os.listdir(path):
-            if filename.endswith('.py') and not filename.startswith('_'):
-                yield filename[:-3]
+    def _find_subclass(self, module, base_class):
+        for attr in dir(module):
+            subclass = getattr(module, attr)
+            if not isinstance(subclass, dict) and subclass != base_class and \
+                    issubclass(subclass, base_class):
+                return subclass
+        raise LookupError('No subclass of %r in %r' % (base_class, module))
 
     def get_controller_for(self, message):
         return self.routes[message.__class__.__name__]
+            # NOTE: we could index by class instead of by class name (str)
 
 
 
