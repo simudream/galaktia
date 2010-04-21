b8680e801f650e01bb2fc8dfead6fe16a083a178
elementohb <elementohb@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-04-21 00:36:02 +0000

    Defined all of the protocol messages and made some changes to Controller in the client.

---
+++ trunk/web/assets/js/protocol/messages/hit.js
@@ -0,0 +1,20 @@
+var Galaktia = Galaktia || {};                                            
+var Galaktia.Messages = Galaktia.Messages || {};      
+
+Galaktia.Messages.HitRequest = new Class({                               
+    Extends: Galaktia.Message,                                         
+    type: 'HitRequestMessage',
+    attributes: ['character_id']
+});
+
+Galaktia.Messages.HitResponse = new Class({                               
+    Extends: Galaktia.Message,                                         
+    type: 'HitResponseMessage',
+    attributes: ['result']
+});
+
+Galaktia.Messages.HitNotification = new Class({                               
+    Extends: Galaktia.Message,                                         
+    type: 'HitNotificationMessage',
+    attributes: ['hitting_character_id', 'hit_character_id', 'result']
+});
