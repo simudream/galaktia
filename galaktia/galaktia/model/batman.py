998896e29a15c0e534b25b5edac7d230464de025
slezica89 <slezica89@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-04-23 00:27:24 +0000

    Added functionality.

--- galaktia/galaktia/model/batman.py
+++ galaktia/galaktia/model/batman.py
@@ -1,4 +1,16 @@
-SARA BARA BARA BARA BATMAN
-SARA BARA BARA BARA BATMAN
-BATMAN
-BAAATMANNN
+
+class Batman:
+	""" SARA BARA BARA BARA BATMAN
+	SARA BARA BARA BARA BATMAN
+	BATMAN
+	BAAATMANNN """
+
+	def __init__(self, *args, **kwargs):
+		if args or kwargs:
+			print "Batman is not bound by parameters."
+
+	def getID(self):
+		return "Bruce Wayne"
+
+	def getVehicle(self):
+		return "Batmobile"
