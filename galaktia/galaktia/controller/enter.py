22531f3afd656cabf534bca487728e2a73e7f51d
rhfixer <rhfixer@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-04-23 01:16:41 +0000

    NEW! Methods.

--- galaktia/galaktia/controller/enter.py
+++ galaktia/galaktia/controller/enter.py
@@ -3,6 +3,7 @@
 
 import logging
 
+from galaktia.model.mapdata import MapData
 from galaktia.model.controller import Controller
 from galaktia.model.message import RequestMessage, ResponseMessage, \
     NotificationMessage
@@ -17,6 +18,8 @@ class EnterResponseMessage(ResponseMessage):
     x = None
     y = None
     character = None
+    dyn_data = [] #
+    sta_data = []
     map_data = [] # iterable with newly seen objects (dynamic and static)
 
 class EnterNotificationMessage(NotificationMessage):
@@ -32,5 +35,24 @@ class EnterController(Controller):
     def handle(self, message):
         # yield EnterResponseMessage(...)
         # yield EnterNotificationMessage(...)
-        raise NotImplementedError('Not yet implemented')
+        user = self.dao.user.get_login_info(message.username, message.password) # magic
+        if user:
+            erm = EnterResponseMessage()
+            char = self.dao.character.get_by_user_id(user.id)[0]
+            sta_data = self.dao.wall.get_near(erm.character, radius=20)
+            map_data = self.dao.ground.get_near(erm.character, radius=20)
+            dyn_data = self.dao.character.get_near(erm.character, radius=20)
+            erm.character, erm.sta_data = char, sta_data
+            erm.map_data, erm.dyn_data = map_data, dyn_data
+            erm._dst_session = char.id
+            yield erm
+            for i in erm.dyn_data:                
+                enm = EnterResponseMessage()
+                enm._dst_session = i.id
+                enm.x, enm.y = char.pos[0:2]
+                enm.character = char
+                yield enm
+        yield None # because scoffey loves yield
+            
+        # raise NotImplementedError('Not yet implemented')
 
