0828fd6d695ac6949e345dd6eddb613f337e16d0
santiago.coffey <santiago.coffey@8f501c24-39b2-11de-a9fe-ad1e22fc3a8a>
2010-05-02 19:38:18 +0000

    Fixed CanvasView rendering for tiles and characters. (Translation, scaling and rotation were not correctly computed for given coordinates.)
    Changes in HTML+CSS+JS to display canvas at full size and resize it on window resize. (The new canvas looks awesome on Google Chrome at full screen!) Now the console HTML element is inserted and fully controlled by the UIController.
    
    Now move requests (MoveRequestMessage) are triggered by arrow key events. (Formerly, by pressing "M".) To visually test this, the sprite is being animated without receiving the MoveResponseMessage.
    
    Created a SceneObjectDAO that holds all objects in current scene.

--- trunk/web/assets/js/main.js
+++ trunk/web/assets/js/main.js
@@ -5,20 +5,22 @@ var Galaktia = {version: '0.2a', client: null};
 Galaktia.setup = function () {
 	// log setup
 	Galaktia.logger = new Log; // see: mootools Log
-	// client + ui + view setup
+	// client + ui + view + dao setup
 	Galaktia.client = new Galaktia.Client();
 	Galaktia.ui = new Galaktia.UIController();
 	Galaktia.view = new Galaktia.CanvasView();
+	Galaktia.dao = new Galaktia.SceneObjectDAO();
+	// console setup (Galaktia.log is a global function accepting messages)
 	Galaktia.log = Galaktia.ui.bindLog(Galaktia.logger);
-			// returns a log callback to be used globally
 	// welcome message
 	Galaktia.log('Welcome Galaktia player! You may play with this '
 			+ 'pre-release-candidate GUI by pressing these keys:'
 			+ '\n\tE\tPrompts for login (Enter Request)'
 			+ '\n\tS\tPrompts for chat (Say Request)'
-			+ '\n\tM\tPrompts for walking (Move Request)'
 			+ '\n\tH\tPrompts for attacking (Hit Request)'
-			+ '\n\tX\tPrompts for logout (Exit Request)');
+			+ '\n\tX\tPrompts for logout (Exit Request)'
+			+ '\n\t\u2190\u2191\u2192\u2193'
+			+ '\tFor walking (Move Request)');
 };
 
 // Main script
